7e4ca19473e7af3da09e58e44db0afa95fab13d7
CIICC-CLTR2 <chris01271988@gmail.com>
2024-11-23 15:51:08 +0000

    table

--- programming_challlenges/Multiplication_Table.java
+++ programming_challlenges/Multiplication_Table.java
@@ -1,26 +1,28 @@
 package programming_challlenges;
+import java.util.Scanner;
 public class Multiplication_Table {
     
     public static void main(String[] args) {
-        
+        int productLenght=10;
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Enter Multiplication table length upto 10 only: ");
+        // productLenght = scan.nextInt();
+
         System.out.println("\n\t\t\t\tMULTIPLICATION TABLE OF 10x10");
-        for(int x=1; x<=10; x++){
+        for(int x=1; x<=productLenght; x++){
             String row="";
             int product=0;
             for(int y=1; y<=10; y++){
-                
                 product = x*y;
                 if(product<10){
-                   row += "\t| "+x*y + " |";
+                   row += "\t|  "+x*y + " |";
                 }
                 else{
                     row += "\t| "+x*y + " |";
                 }
-
-
             }
             System.out.println("\t------------------------------------------------------------------------------\n"+row.toString());
         }
-        System.out.println("\t------------------------------------------------------------------------------\n");
+            System.out.println("\t------------------------------------------------------------------------------\n");
     }
 }
