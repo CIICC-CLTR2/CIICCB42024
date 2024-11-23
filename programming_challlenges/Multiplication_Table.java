68f5a78e951a6710d28f9e769968a11f7da5efa0
CIICC-CLTR2 <chris01271988@gmail.com>
2024-11-23 16:23:36 +0000

    colors

--- programming_challlenges/Multiplication_Table.java
+++ programming_challlenges/Multiplication_Table.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class Multiplication_Table {
     
     public static void main(String[] args) {
+   
         int productLenght=10;
         // Scanner scan = new Scanner(System.in);
         // System.out.print("Enter Multiplication table length upto 10 only: ");
         // productLenght = scan.nextInt();
 
-        System.out.println("\n\t\t\t\tMULTIPLICATION TABLE OF 10x10");
+        System.out.print("\n\u001B[31m\t\t\t\tMULTIPLICATION TABLE OF 10x10\u001B[0m"+"\n");
         for(int x=1; x<=productLenght; x++){
             String row="";
             int product=0;
             for(int y=1; y<=10; y++){
                 product = x*y;
                 if(product<10){
-                   row += "\t|  "+x*y + " |";
+                   row += "\t|\u001B[44m  \u001B[32m\u001B[1m"+x*y + " \u001B[0m|";
                 }
                 else{
-                    row += "\t| "+x*y + " |";
+                    row += "\t|\u001B[44m \u001B[32m\u001B[1m"+x*y + " \u001B[0m|";
                 }
             }
-            System.out.println("\t------------------------------------------------------------------------------\n"+row.toString());
+            System.out.println("\t\u001B[33m------------------------------------------------------------------------------\u001B[0m\n"+row.toString());
         }
             System.out.println("\t------------------------------------------------------------------------------\n");
     }
