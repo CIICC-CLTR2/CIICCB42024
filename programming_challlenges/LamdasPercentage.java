6f8e93ef003e8c240de403765b7596826c3c0a98
CIICC-CLTR2 <chris01271988@gmail.com>
2024-11-29 05:04:18 +0000

    lamdas

--- programming_challlenges/LamdasPercentage.java
+++ programming_challlenges/LamdasPercentage.java
@@ -1,57 +1,48 @@
 package programming_challlenges;
 
+import java.util.Scanner;
+
 public class LamdasPercentage {
-    
- 
-public interface Discounts {
 
-    double getDiscountedPrice(double price,double dis);
-    
-}
- 
+    public interface Discounts {
+
+        double getDiscountedPrice(double price, double dis);
+
+    }
+
     public static void main(String[] args) {
-        // Original price
-        double regPrice = 200.0;
-        double ten_p =.10;
-        double twen_p =.20;
-        double fif_p =.50;
-        double discount=0.0;
 
-        Discounts discounts = (p, d) -> {
-            double a =0.0;
-            if(d == 10){
-                a= regPrice * .10;
-            }
-            else if(d == 20){
-                a= regPrice * .20;
-            }
-            else if(d == 50){
-                a= regPrice * .50;
-            }
-
-            
-                        return a;
-         
-        };
-
-        System.out.println(discounts.getDiscountedPrice(regPrice, 10));
-        System.out.println(discounts.getDiscountedPrice(regPrice, 20));
-        System.out.println(discounts.getDiscountedPrice(regPrice, 50));
-
-        // Discounts discount_of_10 = (price ,a ) -> {price * .10 , price * .20} ;
-     
-        // System.out.println(discount_of_10.getDiscountedPrice(prices, 100));
-
-        // Discounts discount_of_20 = (p) -> p * 0.20;   
-        // Discounts discount_of_50 = (p) -> p * 0.50;  
-
-        // Applying discounts
-        // double priceAfter10Percent = discount_of_10.getDiscount(price);
+        Scanner scan = new Scanner(System.in);
        
+        double regPrice = 0.0;
+        double discount = 0.0;
+        double result=0.0;
 
-        // Printing results
-        // System.out.println("Original price: " + pricse);
-        // System.out.println("Price after 10% discount: " + discount_of_10.getDiscount(prices));
-         
+ 
+
+            System.out.print("Enter Price to be discounted: ");
+            regPrice = scan.nextDouble();
+            
+            System.out.print("Enter discount of [10, 20, 50]% only:  ");
+            discount = scan.nextDouble();
+    
+        
+        Discounts discounts = (p, d) -> {
+            double price = 0.0;
+
+                if (d == 10) {
+                    price = p * d/100;
+                } else if (d == 20) {
+                    price = p * d/100;
+                } else if (d == 50) {
+                    price = p * d/100;
+                }
+            return price  ;
+
+        };
+ 
+        System.out.println((result=discounts.getDiscountedPrice(regPrice, discount))==0.0 ? "invalid input":result);
+    
+ 
     }
 }
