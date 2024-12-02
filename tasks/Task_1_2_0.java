ab68c1f91edb28882de39f4e0dc9642534e430bb
CIICC-CLTR2 <chris01271988@gmail.com>
2024-12-02 05:05:27 +0000

    change some codes

--- tasks/Task_1_2_0.java
+++ tasks/Task_1_2_0.java
@@ -1,26 +1,26 @@
 package tasks;
+import java.util.Arrays;
 
 public class Task_1_2_0 {
 
-    public static double sumOfComulativenumbers(double... numbers) {
+    public static String sumOfComulativenumbers(double... numbers) {
         double sums = 0.0;
+       
         for (double n : numbers) {
             double sum = 0.0;
-            // double num=n;
+         
             for (int x = 1; x < n + x; x++, n--) {
                 sum += n;
-                // System.out.println("The Comulative sum of: "+ num +" - " + n + " is: "+sum);
             }
             sums += sum;
-            // System.out.println("Total sum is: " +sums);
         }
-        // System.out.println(sums);
-        return sums;
+     
+        return "(" + Arrays.toString(numbers) +") is: " +(sums);
     }
 
     public static void main(String[] args) {
 
-        System.out.println("\nThe Comulative Sum of All Parameters is: "+sumOfComulativenumbers(4, 5, 10)+"\n");
+        System.out.println("\nThe Comulative Sum of All Parameters of "+sumOfComulativenumbers(4, 5, 10)+"\n");
 
     }
 }
